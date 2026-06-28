59786cfa9250b6adc64ac59f1406ec2ebbc47045
poorna-24 <chanduterala24@gmail.com>
2026-06-28 16:02:28 +0530

    feat: scaffold NestJS application with Users module, validation DTOs, and initial unit and E2E tests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,83 @@
-import { Controller, Get, Post, Param, Query, Body , Headers , Ip} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Headers,
+  Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  
-  @Get(':id')
-  public getUsers(@Param() params: any, @Query() query:any) {
-    console.log(params);
-    console.log(query);
-      //  console.log(typeof limit,limit)
+  // // GET /users
+  // @Get('/')
+  // public getAllUsers(@Query('limit') limit: any) {
+  //   console.log('id is undefined');
+  //   console.log(limit);
+  //   return `Get all users`;
+  // }
+
+  // // GET /users/:id
+  // @Get('/:id')
+  // public getUserById(@Param('id',ParseIntPipe) id: number | undefined, @Query('limit') limit: any) {
+  //   console.log(typeof id);
+  //   console.log( typeof limit);
+  //   return `Get user by id`;
+  // }
+
+  @Get('/:id')
+  // @Get('/:id/:pp')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    // @Param('pp',ParseIntPipe) pp:number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(id);
+    // console.log(pp);
+
+    // console.log(typeof id);
+    console.log(typeof limit);
+    console.log(typeof page);
+    console.log('chandu');
+    console.log(limit, page);
+    //  console.log(typeof limit,limit)
     // console.log('chandu');
     return `You sent a get request to users endpoint with...........`;
   }
 
+  // // GET /users5
+  // @Get()
+  // public getAllUsers(@Query('limit') limit: any) {
+  //   console.log('id is undefined');
+  //   console.log(limit);
+  //   return `Get all users`;
+  // }
+
+  // @Get(':id')
+  // public getUserss(@Param() params: any, @Query() query: any) {
+  //   console.log(params);
+  //   console.log(query);
+  //   //  console.log(typeof limit,limit)
+  //   // console.log('chandu');
+  //   return `You sent a get request to users endpoint with...........`;
+  // }
+
   @Post()
-  public createUsers(@Body() request:any, @Headers() headers:any, @Ip() ip:any) {
-    console.log(request)
-    console.log(headers)
-    console.log(ip)
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(request);
+    console.log(headers);
+    console.log(ip);
     return ' from POST';
   }
-
 
   // @Get('/:id/{:optional}')
   // public getUsers(
